99af69c3b2eb469749209f6661505fce553d00c6
riteshxoni <riteshsony2@gmail.com>
2025-08-22 15:51:52 +0530

    fixing

--- src/components/TIPETemplate.jsx
+++ src/components/TIPETemplate.jsx
@@ -7,9 +7,9 @@ const TIPETemplate = ({data}) =>
                     <div className='card p-2 text-center' style={{fontSize:'.8rem',minHeight:450,maxHeight:450}}>
                         <img src={item.image} className='img-fluid'/>
                         <div className="card-body">
-                            <h5 className="card-title" style={{minHeight:70}}>{item.title}</h5>
+                            <h5 className="card-title" >{item.title}</h5>
                             <p className="card-text text-justify-custom " >{item.text}</p>
-                            <div className="row justify-content-evenly" style={{minHeight:30}}>
+                            <div className="row justify-content-evenly" >
                                 <div className="col-6">{item.from}</div>
                                 <div className="col-6">{item.to}</div>
                             </div>
