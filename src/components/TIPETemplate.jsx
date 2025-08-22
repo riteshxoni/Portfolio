703272ee5645dbdfe9a238154eff58d3f5543246
riteshxoni <riteshsony2@gmail.com>
2025-08-22 15:17:55 +0530

    fixed height issue

--- src/components/TIPETemplate.jsx
+++ src/components/TIPETemplate.jsx
@@ -3,8 +3,8 @@ const TIPETemplate = ({data}) =>
   return (
     <div className="row justify-content-evenly">
             {data.map((item,index) => (
-                <div key={index} className="col-6 col-md-4 col-lg-3 mt-2" style={{minHeight:400,maxHeight:400}}>
-                    <div className='card p-2 text-center' style={{fontSize:'.8rem',minHeight:400,maxHeight:400}}>
+                <div key={index} className="col-6 col-md-4 col-lg-3 mt-2" style={{minHeight:450,maxHeight:450}}>
+                    <div className='card p-2 text-center' style={{fontSize:'.8rem',minHeight:450,maxHeight:450}}>
                         <img src={item.image} className='img-fluid'/>
                         <div className="card-body">
                             <h5 className="card-title">{item.title}</h5>
