4af4e54b675ce10eff2c1564c62824b1edf4bf4d
riteshxoni <riteshsony2@gmail.com>
2025-08-22 15:33:17 +0530

    fixed things

--- src/components/TIPETemplate.jsx
+++ src/components/TIPETemplate.jsx
@@ -1,15 +1,15 @@
 const TIPETemplate = ({data}) => 
     {
   return (
-    <div className="row justify-content-evenly">
+    <div className="row justify-content-evenly align-items-center" >
             {data.map((item,index) => (
-                <div key={index} className="col-6 col-md-4 col-lg-3 mt-2" style={{minHeight:450,maxHeight:450}}>
+                <div key={index} className="col-6 col-md-4 col-lg-3 mt-2" >
                     <div className='card p-2 text-center' style={{fontSize:'.8rem',minHeight:450,maxHeight:450}}>
                         <img src={item.image} className='img-fluid'/>
                         <div className="card-body">
-                            <h5 className="card-title">{item.title}</h5>
-                            <p className="card-text text-justify-custom ">{item.text}</p>
-                            <div className="row justify-content-evenly">
+                            <h5 className="card-title" style={{minHeight:70}}>{item.title}</h5>
+                            <p className="card-text text-justify-custom " >{item.text}</p>
+                            <div className="row justify-content-evenly" style={{minHeight:30}}>
                                 <div className="col-6">{item.from}</div>
                                 <div className="col-6">{item.to}</div>
                             </div>
